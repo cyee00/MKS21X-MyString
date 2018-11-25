526156359bdc94a20a4d13b025ee8af0f103c9c1
cyee00 <cyee00@stuy.edu>
2018-11-25 00:27:19 -0500

    m

--- MyString.java
+++ MyString.java
@@ -1,10 +1,10 @@
-public class MyString implements CharSequence{
+public class MyString implements CharSequence,Comparable<CharSequence>{
 
   private char[] chars;
   private CharSequence cs;
 
-  public MyString(CharSequence cs){
-    cs = cs;
+  public MyString(CharSequence charsequence){
+    cs = charsequence;
     chars = new char[cs.length()];
     for (int i=0;i<cs.length();i++){
       chars[i]=cs.charAt(i);
@@ -44,6 +44,25 @@ public class MyString implements CharSequence{
       ans+=cs.charAt(i);
     }
     return ans;
+  }
+
+
+  public int shorter(CharSequence a, CharSequence b){ //helpr function for compareTo; returns the length of the shorter of the two charsequences so I don't get an IndexOutOfBoundsException
+    if (a.length()>b.length()){
+      return a.length();
+    }
+    return b.length();
+  }
+
+  public int compareTo(CharSequence c){
+    for (int i=0;i<shorter(cs,c);i++){
+      if (cs.charAt(i)+0>c.charAt(i)){
+        return 1;
+      } else if (c.charAt(i)+0>cs.charAt(i)+0){
+        return -1;
+      }
+    }
+    return 0;
   }
 
 }
