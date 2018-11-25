bf8ec14e900ef08d18d67a775ac461fd87b39876
cyee00 <cyee00@stuy.edu>
2018-11-25 01:19:15 -0500

    comments

--- MyString.java
+++ MyString.java
@@ -6,23 +6,23 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
   public MyString(CharSequence charsequence){
     cs = charsequence;
     chars = new char[cs.length()];
-    for (int i=0;i<cs.length();i++){
+    for (int i=0;i<cs.length();i++){//loop through the charsequence and make an array of each individual character
       chars[i]=cs.charAt(i);
     }
   }
 
   public char charAt(int index){
     for (int i=0;i<cs.length();i++){
-      if (i==index){
+      if (i==index){//loop through the charsequence until you get to the desired index (if it exists)
         return chars[i];
       }
     }
-    throw new IndexOutOfBoundsException();
+    throw new IndexOutOfBoundsException();//if the desired index doesn't exist, throw the exception
   }
 
   public int length(){
     int ans = 0;
-    for (int i=0;i<chars.length;i++){
+    for (int i=0;i<chars.length;i++){//loop through the array and add 1 for every char in it
       ans++;
     }
     return ans;
@@ -30,11 +30,11 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
 
   public CharSequence subSequence(int start, int end){
     String ans = "";
-    if (start<0||end<0||end>cs.length()||start>end){
+    if (start<0||end<0||end>cs.length()||start>end){//covers all the cases stated on docs.oracle.com
       throw new IndexOutOfBoundsException();
     }else{
       for (int i=0;i<cs.length();i++){
-        if (i>=start&&i<end){
+        if (i>=start&&i<end){//restricts the answer so that it only contains chars from indexes within the boundaries given by user, thus "subsequencing" it
           ans+=cs.charAt(i);
         }
       }
@@ -60,20 +60,27 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
 
   public int compareTo(CharSequence c){
     if (cs==null||c==null){
-      throw new NullPointerException();
-    }
-    if (cs==""&&c!=""){
+      throw new NullPointerException(); //covers the NullPointerException thrown by compareTo
+    } else if (cs==""&&c!=""){ //since an empty string will lead to an error in my for loop, I covered the specific cases here
       return -1;
     } else if (c!=""&&cs==""){
       return 1;
-    } else for (int i=0;i<shorter(cs,c);i++){
-      if (cs.charAt(i)+0>c.charAt(i)+0){
+    } else if (cs==""&&c==""){
+      return 0;
+    } else for (int i=0;i<shorter(cs,c);i++){ //loops through the shorter of the two charsequences
+      if (cs.charAt(i)+0>c.charAt(i)+0){//the +0 converts the char to its int value
         return 1;
       } else if (c.charAt(i)+0>cs.charAt(i)+0){
         return -1;
-      }
+      }//if the characters are =, nothing happens
     }
-    return 0;
+    //if the code has made it to this point, it means one of three things:
+    if (cs.length()>c.length()){//identical up to a point, but THIS charsequence has more characters
+      return 1;
+    } else if (c.length()>cs.length()){//identical up to a point, but OTHER charsequence has more characters
+      return -1;
+    }
+    return 0;//the two charsequences are identical.
   }
 
 }
